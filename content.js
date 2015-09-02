560e89a1c235a3d85e840e3eb86d306fb4df5f8d
CoryXie <cory.xie@gmail.com>
2015-09-02 12:06:08 +0800

    Prevent summary page image from being used.

--- content.js
+++ content.js
@@ -13,65 +13,73 @@ function toggleVisibility() {
         el.show();
     } else {
         el.hide();
-    }	
-	
+    }
+
     var allImages = $(".patent-thumbnail");
+
+    if (allImages === null)
+        return;
+
     var displays = [];
 
     for (var i = 0; i < allImages.length; i++) {
         var image = allImages[i];
-        //console.log(image.innerHTML);
-        //console.log($(image.innerHTML).attr("href"));
+        var href = $(image.innerHTML).attr("href");
+		
+        //the first image maybe the summary page image which should be removed
+        var match = href.match(/\d+\.png/g)[0];
+        if (match === "00000.png")
+            continue;
+
         displays.push({
             count: 0,
-            html: "<img style=\"height:725px;max-width:725px;width: expression(this.width > 725 ? 725: true);\" src=\"" + $(image.innerHTML).attr("href") + "\"/>"
+            html: "<img style=\"height:725px;max-width:725px;width: expression(this.width > 725 ? 725: true);\" src=\"" + href + "\"/>"
         });
     }
 
     var descSections = $("div[class*='patent-section patent-description-section'] p");
 
     descSections.each(function(index) {
-		
-        //console.log(this.innerText);
-		
-		// do not insert images into the description-of-drawings section
-		if ($(this).parent('description-of-drawings').length)
-			return;
-		
+
+        // do not insert images into the description-of-drawings section
+        if ($(this).parent('description-of-drawings').length)
+            return;
+
         var matches = this.innerText.match(/FIG. \d+/g);
-		
-		if (matches === null)
-			return;
-		
-		for (var m = 0; m < matches.length; m++) {
-			var figure = matches[m];
-			
-			//console.log(figure);
-			
-			var image = figure.match(/\d+/g)[0] - 1;
-			
-			if (image >= allImages.length)
-				break;
-			
-			if (displays[image].count++ < 2)
-				$(this).append(displays[image].html);
-		}
+
+        if (matches === null)
+            return;
+
+        for (var m = 0; m < matches.length; m++) {
+            var figure = matches[m];
+
+            //try our best luck to assue index match 
+            var image = figure.match(/\d+/g)[0] - 1;
+
+            if (image >= displays.length)
+                break;
+
+            if (displays[image].count === 0) {
+                displays[image].count = 1;
+                $(this).append(displays[image].html);
+            }
+        }
     });
-	
-	
-	var angle = 0;
-	$("img").rotate({
-	    bind: {
-	        click: function() {
-				angle +=90;
-				if (angle === 450)
-					angle = 0;
-	            $(this).rotate({	                
-	                angle: angle
-	            })
-	        }
-	    }
-	});
+
+
+    var angle = 0;
+    $("img").rotate({
+        bind: {
+            click: function() {
+                angle += 90;
+                if (angle === 450)
+                    angle = 0;
+                $(this).rotate({
+                    angle: angle
+                })
+            }
+        }
+    });
 }
 
 toggleVisibility();
